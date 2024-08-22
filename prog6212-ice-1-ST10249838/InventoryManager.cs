b733deac75482c38eb73a46ca3b33af775d0fb26
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-08-22 15:27:32 +0200

    Add InventoryManager class to ICE1 namespace
    Introduce InventoryManager class in ICE1 namespace to manage
    Product objects. Added methods for adding, retrieving, updating,
    and processing sales of products. Also includes a method to
    generate an inventory report.

---
+++ prog6212-ice-1-ST10249838/InventoryManager.cs
@@ -0,0 +1,46 @@
+﻿namespace ICE1
+{
+    public class InventoryManager
+    {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public Product? GetProductById(int productId)
+        {
+            return products.FirstOrDefault(p => p.ProductID == productId);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = GetProductById(product.ProductID);
+            if (existingProduct != null)
+            {
+                products.Remove(existingProduct);
+                products.Add(product);
+            }
+        }
+
+        public void ProcessSale(int productId, int quantity)
+        {
+            var product = GetProductById(productId);
+            if (product != null)
+            {
+                product.ProcessSale(product, quantity);
+            }
+            else
+            {
+                throw new Exception("Product not found.");
+            }
+        }
+
+        public IEnumerable<dynamic> GenerateReport()
+        {
+            return from p in products
+                   select new { p.Name, p.Category, p.StockQuantity };
+        }
+    }
+}
